dd7d86034a08aec2d7ecba3086f2a77f2b4d4473
Carson Holgate <clholgat@ncsu.edu>
2010-09-24 15:29:15 -0400

    merging everything

---
+++ sample_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '8429448037446541f4b4cbc98104ff774b99ae619645963144272e962e5a6e244f714b22644cd6d972954512ea23b11dcc505094af376f59d5347c954a85b634'
